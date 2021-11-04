52694b39c13f5adb58be54e485a17042386fda7a
Manuel Mendoza <mj.mendoza.trabajo@gmail.com>
2021-11-04 23:36:14 +0100

    create router

--- app/app.js
+++ app/app.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const express = require('express');
 
 const routerAppoinments = require('./appoinments/router');
-const routerClients = require('./clients/router');
+// const routerClients = require('./clients/router');
 
 app = express();
+app.use(express.json());
 app.use("/appoinments", routerAppoinments);
-app.use("/client", routerClients);
+// app.use("/client", routerClients);
 
 app.listen(process.env.PORT, () => console.log('El servidor esta levantado en', process.env.PORT));
---
+++ app/clients/controller.js
@@ -0,0 +1,2 @@
+const express = require('express');
+const Clients = require('./clientsmodel');
