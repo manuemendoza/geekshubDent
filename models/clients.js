f95ce7610a2f1ab4bd7018b939c650a9509cd472
Stefano Martin <see.martiinez@gmail.com>
2021-11-05 17:45:57 +0100

    añadimos funciones de config

--- models/clients.js
+++ models/clients.js
@@ -1,8 +1,5 @@
 'use strict';
-const {
-
-    Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class ClientModel extends Model {
         /**
@@ -24,4 +21,4 @@ module.exports = (sequelize, DataTypes) => {
         sequelize,
         modelName: 'ClientModel',
     });
-    return ClientModel;
+    // return ClientModel;
