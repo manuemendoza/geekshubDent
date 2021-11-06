95262c5e5a95f826fd942dbe308fa97bd8446322
GustavoSierra <gsierra.business@gmail.com>
2021-11-06 20:44:30 +0100

    appointment update and delete done

--- app/appoinments/controller.js
+++ app/appoinments/controller.js
@@ -42,6 +42,70 @@ appoinmentCont.getById = (req, res) => {
           message: "Ha surgido algún error al intentar acceder a la cita con el id " + id + "."
         });
       });
+};
+
+appoinmentCont.update = (req, res) => {
+
+    if (req.user.usuario.role == "admin") {
+  
+            const id = req.params.id;
+  
+            Appoinment.update(req.body, {
+              where: { id: id }
+            })
+              .then(num => {
+                if (num == 1) {
+                  res.send({
+                    message: "La cita ha sido actualizada correctamente."
+                  });
+                } else {
+                  res.send({
+                    message: `No se ha podido actualizar la cita con el id ${id}`
+                  });
+                }
+              })
+              .catch(err => {
+                res.status(500).send({
+                  message: "Ha surgido algún error al intentar actualizar la cita con el id " + id + "."
+                });
+              });
+    }else{
+      res.send({
+        message: `No tienes permisos para actualizar la información de la cita. Contacta con un administrador.`
+      });
+    }
+  };
+
+  appoinmentCont.delete = (req, res) => {
+
+    if (req.body.Client.rol == "admin" || req.user.Client.id == req.body.clientId) {
+  
+          const id = req.params.id;
+  
+          Appoinment.destroy({
+              where: { id: id }
+          })
+              .then(num => {
+                  if (num == 1) {
+                      res.send({
+                          message: `La cita con id ${id} ha sido eliminada correctamente.`
+                      });
+                  } else {
+                      res.send({
+                          message: `No se ha podido eliminar la cita con id ${id}.`
+                      });
+                  }
+              })
+              .catch(err => {
+                  res.status(500).send({
+                      message: "Ha surgido algún error al intentar borrar la cita con el id " + id
+                  });
+              });
+    }else{
+      res.send({
+        message: `No tienes permisos para borra la cita.`
+      });
+    }
   };
 
 // const createAppoinment = async(req, res) => {
