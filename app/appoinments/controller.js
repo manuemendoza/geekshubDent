71cfaed67f6bac48fb76b95fe95090285fc209d4
Stefano Martin <see.martiinez@gmail.com>
2021-11-06 18:35:55 +0100

    We add function to delete users and admins

--- app/appoinments/controller.js
+++ app/appoinments/controller.js
@@ -1,58 +1,55 @@
-
 const { Appoinment } = require('../../models/index');
 const { Client } = require('../../models/index')
 
 const appoinmentCont = {};
 
-appoinmentCont.getAll = (req, res) => {
-    try{
-        Appoinment.findAll()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-            message:
-                err.message || "Ha surgido algún error al intentar acceder a las citas."
-            });
-        });
-    }
-    catch{
-        res.json({
-            message: 'Algo ha ido mal'
-        })
-    }
-};
-
-appoinmentCont.getById = (req, res) => {
-    const id = req.params.id;
-  
-    Appoinment.findByPk(id)
-      .then(data => {
-        if (data) {
-          res.send(data);
-        } else {
-          res.status(404).send({
-            message: `No existe la cita con el id ${id}.`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Ha surgido algún error al intentar acceder a la cita con el id " + id + "."
-        });
-      });
-  };
-
-// const createAppoinment = async(req, res) => {
-//     const appoinment = new Appoinments(req.body);
-//     console.log(appoinment);
-//     await appoinment.save()
-//     res.json()
+// appoinmentCont.getAll = (req, res) => {
+//     try{
+//         Appoinment.findAll()
+//         .then(data => {
+//             res.json(data);
+//         })
+//         .catch(err => {
+//             res.status(500).send({
+//             message:
+//                 err.message || "Ha surgido algún error al intentar acceder a las citas."
+//             });
+//         });
+//     }
+//     catch{
+//         res.json({
+//             message: 'Algo ha ido mal'
+//         })
+//     }
 // };
 
-// module.exports = {
-//     createAppoinment,
-//     appoinmentCont
+// appoinmentCont.getById = (req, res) => {
+//     const id = req.params.id;
+  
+//     Appoinment.findByPk(id)
+//       .then(data => {
+//         if (data) {
+//           res.send(data);
+//         } else {
+//           res.status(404).send({
+//             message: `No existe la cita con el id ${id}.`
+//           });
+//         }
+//       })
+//       .catch(err => {
+//         res.status(500).send({
+//           message: "Ha surgido algún error al intentar acceder a la cita con el id " + id + "."
+//         });
+//       });
+//   };
 
-// }
+const createAppoinment = async(req, res) => {
+    const appoinment = new Appoinments(req.body);
+    console.log(appoinment);
+    await appoinment.save()
+    res.json()
+};
+
+module.exports = {
+    createAppoinment,
+}
--- app/clients/controller.js
+++ app/clients/controller.js
@@ -37,12 +37,37 @@ const createUser = (req, res) => {
         //         }, 500);
         //     }
         // }
+        const deleteUser = async(req, res) => {
+            const primaryK = req.params.id
+            try {
+                const user = await Client.findByPk(primaryK);
+                if (user) {
+                    res.json(user);
+                    await user.destroy(
+                    {
+                        where: { id : primaryK}
+                    }
+                    );
+                } else {
+                    res.json({
+                        messege: 'user not found'
+                    }, 404)
+                }
+            } catch (error) {
+                console.error(error);
+                res.json({
+                    message: error.message
+                }, 500);
+            }
+        };
     };
 
     // const userData = req.body
     // console.log(typeof userData);
     // const prueba = Client.create(userData);
     // res.json(prueba);
+
+
 }
 
 module.exports = {
