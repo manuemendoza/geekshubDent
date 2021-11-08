3ea0e0e8eac76b0109fc33f39fb434be22e8c5bb
GustavoSierra <gsierra.business@gmail.com>
2021-11-08 20:24:08 +0100

    appointment getbyid

--- app/appoinments/controller.js
+++ app/appoinments/controller.js
@@ -39,7 +39,7 @@ const appoinmentGetById = async(req, res) => {
         const id = req.params.id;
         try{ 
         const busqueda = await Appointment.findByPk(id)
-            if (id) {
+            if (busqueda) {
               res.json(busqueda);
             } else {
               res.status(404).send({
@@ -52,38 +52,6 @@ const appoinmentGetById = async(req, res) => {
             });
           }
   };
-  
-// const appoinmentUpdate = (req, res) => {
-
-//     if (req.user.Admin.role == "admin") {
-  
-//             const id = req.params.id;
-  
-//             Appointment.update(req.body, {
-//               where: { id: id }
-//             })
-//               .then(num => {
-//                 if (num == 1) {
-//                   res.send({
-//                     message: "La cita ha sido actualizada correctamente."
-//                   });
-//                 } else {
-//                   res.send({
-//                     message: `No se ha podido actualizar la cita con el id ${id}`
-//                   });
-//                 }
-//               })
-//               .catch(err => {
-//                 res.status(500).send({
-//                   message: "Ha surgido algún error al intentar actualizar la cita con el id " + id + "."
-//                 });
-//               });
-//     }else{
-//       res.send({
-//         message: `No tienes permisos para actualizar la información de la cita. Contacta con un administrador.`
-//       });
-//     }
-//   };
 
 // const appoinmentDelete = (req, res) => {
 
@@ -121,6 +89,5 @@ module.exports = {
   appoinmentCreate,
   appoinmentGetAll,
   appoinmentGetById
-  // appoinmentUpdate,
   // appoinmentDelete
 };
--- app/appoinments/router.js
+++ app/appoinments/router.js
@@ -4,7 +4,6 @@ const controller = require('./controller');
 router.get('/:id', controller.appoinmentGetById);
 router.get('/', controller.appoinmentGetAll);
 router.post('/', controller.appoinmentCreate);
-// router.put('/:id', controller.appoinmentUpdate);
 // router.delete('/:id', controller.appoinmentDelete);
 
 module.exports = router;
