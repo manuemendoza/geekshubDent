74db00f2e932f0a7e1b559886c22022f44285e0d
GustavoSierra <gsierra.business@gmail.com>
2021-11-10 09:59:36 +0100

    associations between admin and token

--- models/token.js
+++ models/token.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            Client.belongsTo(models.Client, {
+            Token.belongsTo(models.Client, {
+                allowNull: false
+            })
+            Token.belongsTo(models.Admin, {
                 allowNull: false
             })
         }
