c675840feb6b50c2bf0c5e017d1a11b77fddf62d
Manuel Mendoza <mj.mendoza.trabajo@gmail.com>
2021-11-28 22:44:08 +0100

    modify clients router

--- app/clients/router.js
+++ app/clients/router.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
 
 const controller = require('./controller');
-const auth = require('../auth.js');
+const auth = require('../auth');
 
-router.get('/:id', auth.checkAdminOrOwn, controller.getUser);
-router.get('/', auth.checkClient, controller.getUsers);
-router.post('/', controller.createUser);
-router.post('/login', controller.loginUser);
-router.post('/logout', auth.checkClient, controller.logoutUser);
-router.put('/:id', auth.checkClient, controller.updateUser);
-router.delete('/:id', auth.checkAdminOrOwn, controller.deleteUser);
-
+router.get('/:id',auth.checkAdminOrOwn, controller.getClient);
+router.get('/',auth.checkClient, controller.getClients);
+router.post('/', controller.createClient);
+router.post('/login', controller.loginClient);
+router.post('/logout', auth.checkClient, controller.logoutClient);
+router.put('/:id', auth.checkAdminOrOwn, controller.updateClient);
+router.delete('/:id', auth.checkAdminOrOwn, controller.deleteClient);
 
 
 module.exports = router;
